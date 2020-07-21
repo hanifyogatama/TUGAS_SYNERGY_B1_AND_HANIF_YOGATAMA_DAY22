f1932bfee4de20382bec89e50b3b34fe92c8542c
Hanif yogatama <hanifyotama@gmail.com>
2020-07-21 21:42:36 +0700

    add photo profile, post, followers, following, name, bio, hightlight

--- app/src/main/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day22/MainActivity.kt
+++ app/src/main/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day22/MainActivity.kt
@@ -2,10 +2,38 @@ package com.binar.tugas_synergy_b1_and_hanif_yogatama_day22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+            .load(R.drawable.oppa)
+            .circleCrop()
+            .into(ivProfile)
+
+        Glide.with(this)
+            .load(R.drawable.binar)
+            .circleCrop()
+            .into(ivHighlight1)
+
+        Glide.with(this)
+            .load(R.drawable.jakarta)
+            .circleCrop()
+            .into(ivHighlight2)
+
+        Glide.with(this)
+            .load(R.drawable.binar)
+            .circleCrop()
+            .into(ivHighlight3)
+
+        Glide.with(this)
+            .load(R.drawable.jakarta)
+            .circleCrop()
+            .into(ivHighlight4)
     }
 }
