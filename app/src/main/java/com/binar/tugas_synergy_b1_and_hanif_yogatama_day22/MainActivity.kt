ce0d5f87431d65f24c15c2cbb54c57b68f0f68cc
Hanif yogatama <hanifyotama@gmail.com>
2020-07-21 15:58:26 +0700

    first commit

---
+++ app/src/main/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day22/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.tugas_synergy_b1_and_hanif_yogatama_day22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day22/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.binar.tugas_synergy_b1_and_hanif_yogatama_day22
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
